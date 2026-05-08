bf2a74c0e0adaddcf699ba856f7ef045216000d7
CHRISTIANO PICCININ <christiano.piccinin26@univille.br>
2026-05-07 21:47:09 -0300

    adicionadno function

--- src/function_app.py
+++ src/function_app.py
@@ -6,16 +6,13 @@ app = func.FunctionApp()
 @app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def timer_trigger_aula02(myTimer: func.TimerRequest) -> None:
-    if myTimer.past_due:
-        logging.info('The timer is past due!')
+    logging.info('aula 2.')
 
-    logging.info('Python timer trigger function executed.')
+
+
+
 
 @app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def timer_trigger_aula03(myTimer: func.TimerRequest) -> None:
-    
-    if myTimer.past_due:
-        logging.info('The timer is past due!')
-
-    logging.info('Python timer trigger function executed.')
+    logging.info('aula 3.')
